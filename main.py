da8dcfef1557821cadec09058dc5e3711f6d4010
bri-boop <briana.brooks@outlook.com>
2023-02-24 14:22:31 -0800

    fixed undefined variable, need to switch functions

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import random
 
+destination_list=['london', 'tokyo', 'portland']
+resturant_list = ['fish and chips', 'sushi', 'voodoo doughnuts']
+transportation_list = ['double decker' , 'shinkansen', 'uber']
+entertainment_list =['shipping ' , 'hot springs' ,  'music festival']
+
+
 def print_greeting():
     print('Thank you for choosing the Day Trip Generator')
     print("-----------------------------------------------")
@@ -11,9 +17,9 @@ def pick_from_list(list_to_pick_from, topic):
     user_input = input (f'How about going to {random_item} for {topic}? Y/N')
     while user_input != "Y":
         random_item = random.choice(list_to_pick_from)
-        user_input = input (f'How about going to {random_item} for {topic}? Y/N')
+        user_input = input (f'How about eating {random_item} for {topic}? Y/N')
     return random_item
-
+    while user_input:"Y":print ("Great! Onto food!")
 
 def run_trip_genarator():
     destination_list = ['london', 'tokyo', 'portland']
@@ -28,4 +34,4 @@ final_trip = {"destination": "",
 print_greeting()
 final_trip["destination"]=pick_from_list(destination_list, "destination")
 print(final_trip['destination'])
-run_trip_genarator                                                                                                                                                                                                                                                                                              
+final_trip["resturant"]=pick_from_list(resturant_list, "resturant")
